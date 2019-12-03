55710c23525fa6f7127ae04828d09eed4c3975f9
Trenton Green <trentonagreen@gmail.com>
2019-12-02 20:37:20 -0800

    Upload of basic 3rd person controller project w/ basic enemy AI

---
+++ Test/New Unity Project/Assets/Scripts/Enemy/Enemy_Controller.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_Controller : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public Transform player;
+    public float moveSpeed;
+    public float range;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        // Look at player target
+        transform.LookAt(player);
+
+        Vector3 playerDirection = transform.forward * moveSpeed * Time.fixedDeltaTime;
+
+        // if distance between enemy position and player position is less than the range
+        //      move enemy position to player position
+        if (Vector3.Distance(transform.position, player.position) <= range)
+        {
+            rb.MovePosition(transform.position + playerDirection);
+        }
+    }
+}
---
+++ Test/New Unity Project/Assets/Scripts/Player/Player_Controller.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Controller : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Transform cam;
+
+    private Vector3 direction;
+    private Vector3 rotationDirection;
+
+    public float moveSpeed;
+    public float rotationSpeed;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        cam = Camera.main.transform;
+    }
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        #region Walking
+        Vector3 forward = cam.forward;
+        Vector3 right = cam.right;
+        forward.y = 0;
+        right.y = 0;
+
+        direction = horizontal * right + vertical * forward;
+        direction = direction * moveSpeed * Time.fixedDeltaTime;
+
+        rb.MovePosition(transform.position + direction);
+        #endregion
+
+        #region Rotation
+        rotationDirection = horizontal * right + forward * vertical;
+
+        if (!Mathf.Approximately(horizontal, 0.0f) || !Mathf.Approximately(vertical, 0.0f))
+        {
+            float angle = Quaternion.Angle(transform.rotation, Quaternion.LookRotation(rotationDirection));
+
+            if (!Mathf.Approximately(angle, 0.0f))
+            {
+                Quaternion deltaRotation = Quaternion.RotateTowards(rb.rotation, Quaternion.LookRotation(rotationDirection), rotationSpeed * Time.fixedDeltaTime);
+                rb.MoveRotation(deltaRotation);
+            }
+        }
+        #endregion
+    }
+}
